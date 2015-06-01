b6ae2766dc43b14823d9e4f0aa43ccb21ae18c30
Karim Alibhai <k4rim.sa@gmail.com>
2015-06-01 17:07:08 -0400

    separate wizard code

--- app/js/index.js
+++ app/js/index.js
@@ -8,7 +8,10 @@ process.app.controller('main', ['$scope', function ($scope) {
 
   'use strict';
 
-  var priority = require('../lib/priority');
+  var priority = require('../lib/priority'),
+      strategies = require('../lib/strategy-controller')([
+        'find'
+      ]);
 
   // this switch allows us to toggle between the UI
   // displaying the master's info or a given player's
@@ -18,6 +21,11 @@ process.app.controller('main', ['$scope', function ($scope) {
   // this priority queue maintains the possibilities
   // of the murder, and their respective probability
   $scope.masterGuess = priority();
+  $scope.masterDefinite = global.masterDefinite = {
+    people: [],
+    places: [],
+    weapons: []
+  };
 
   // the player global will be the current selected
   // player in the UI, so we don't have to modify a whole
@@ -27,34 +35,6 @@ process.app.controller('main', ['$scope', function ($scope) {
   // we use an array + an object to maintain the list of
   // players and their data so we can maintain and manipulate
   // order
-  $scope.players = [];
+  $scope.players = global.players = [];
   $scope.playerdata = {};
-
-  // .addPlayer([name])
-  // adds a player to the game (in the next player position)
-  // -> player position can be modified through the UI
-  $scope.addPlayer = function (player) {
-    $scope.players.push(player);
-    $scope.playerdata[player] = {
-      maybe: priority('length'),
-      sure: []
-    };
-  };
-
-  // .rmPlayer([index])
-  // remove a player at a specific position
-  // using a position over a name makes it easier for angular
-  $scope.rmPlayer = function (at) {
-    var who = $scope.players[at];
-    $scope.players = $scope.players.slice(0, at).concat($scope.players.slice(at + 1));
-    delete $scope.playerdata[who];
-  };
-
-  // .load([name])
-  // set the given player as the global/current player
-  $scope.load = function (name) {
-    $scope.isMaster = false;
-    $scope.player = $scope.playerdata[name];
-  };
-
 }])
--- app/js/wizard.js
+++ app/js/wizard.js
@@ -1,3 +1,39 @@
 /**
  * js/wizard.js - detective
+ * all things binding and setup for the wizard.
+ *
+ * Licensed under GPLv3.
+ * Copyright (C) 2015 VBKA.
  **/
+
+process.app.controller('wizard', ['$scope', function ($scope) {
+  $scope.players = global.players;
+  $scope.masterDefinite = global.masterDefinite;
+
+  // .players.add([name])
+  // adds a player to the game (in the next player position)
+  // -> player position can be modified through the UI
+  $scope.players.add = function (player) {
+    $scope.players.push(player);
+    $scope.playerdata[player] = {
+      maybe: priority('length'),
+      sure: []
+    };
+  };
+
+  // .players.rm([index])
+  // remove a player at a specific position
+  // using a position over a name makes it easier for angular
+  $scope.players.rm = function (at) {
+    var who = $scope.players[at];
+    $scope.players = $scope.players.slice(0, at).concat($scope.players.slice(at + 1));
+    delete $scope.playerdata[who];
+  };
+
+  // .load([name])
+  // set the given player as the global/current player
+  $scope.players.load = function (name) {
+    $scope.isMaster = false;
+    $scope.player = $scope.playerdata[name];
+  };
+}])
