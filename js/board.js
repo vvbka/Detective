16f76446bccdac6e5462724f0c1010d5842cb01a
Karim Alibhai <k4rim.sa@gmail.com>
2015-06-12 18:24:52 -0400

    add doors

--- js/board.js
+++ js/board.js
@@ -4,20 +4,44 @@
  **/
 
 process.app.controller('BoardController', function ($scope, $global) {
-  'use strict';
-  
-  $scope.board = require('./data/board.json');
-  $scope.labels = {
-    '1,2': 'Study',
-    '3,11': 'Hall',
-    '3,19': 'Lounge',
-    '11,18': 'Dining Room',
-    '20,20': 'Kitchen',
-    '20,10': 'Ballroom',
-    '20,1': 'Conservatory',
-    '13,1': 'Billiard Room',
-    '8,2': 'Library',
-    
-    
-  };
+    'use strict';
+
+    var Detective = $global.Detective,
+        board = require('./data/board.json'),
+        stratctl = require('./lib/strategy-controller')($global, [
+            'find'
+        ]);
+
+    $scope.board = board.board;
+    $scope.labels = board.labels;
+    $scope.doors = board.doors;
+
+    // add room resolution to Detective
+    Object.defineProperty(Detective, 'room', {
+        get: function () {
+            var loc = Detective.location.join(',');
+
+            for (var room in $scope.doors) {
+                if ($scope.doors.hasOwnProperty(room)) {
+                    for (var door of $scope.doors[room]) {
+                        if (door.join(',') === loc) return room;
+                    }
+                }
+            }
+
+            return null;
+        }
+    });
+
+    // alfred binding for turn handling
+    $global.myTurn = function () {
+        stratctl.getBest(function (result) {
+            console.log('result = ' + JSON.stringify(result));
+            console.log('Detective.room = ' + Detective.room);
+
+            if (result.destination === Detective.room) {
+                // ...
+            }
+        });
+    };
 });
