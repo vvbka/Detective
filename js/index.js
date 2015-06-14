e91d6005ff566077a35071578960dfac88bd7cf1
Valen Varangu Booth <valen@valenvb.ca>
2015-06-14 19:19:44 -0400

    also have to add that filter here

--- js/index.js
+++ js/index.js
@@ -218,7 +218,7 @@ process.app.controller('main', function ($scope, $global) {
                     // Step #6: Check Answerer's maybes for any row with only one item.
                     // Step #7: For each such row:
                     while ($global.players[answerer].maybe.length > 0 && $global.players[answerer].maybe.first().filter(function(n){ return n != undefined }).length < 2) {
-                        var first = $global.players[answerer].maybe.pop()[0];
+                        var first = $global.players[answerer].maybe.filter(function(n){ return n != undefined }).pop()[0];
 
                         // Step #7.1: Add to Answerer's definite.
                         $global.players[answerer].sure.push(first);
