181fd230b96b61214e91090decac16455ba60e3b
Valen Varangu Booth <valen@valenvb.ca>
2015-06-15 12:21:50 -0400

    Add Position settign cancel

--- js/board.js
+++ js/board.js
@@ -330,8 +330,8 @@ process.app.controller('BoardController', function ($scope, $global) {
         }.bind(this));
     }.bind(this);
     
+    //Hand;ting the movement of other players
     this.movePlayer = $global.movePlayer = function(player){
-        
         $scope.activeplayer = player;
         try{
             $scope.$apply()
@@ -348,6 +348,13 @@ process.app.controller('BoardController', function ($scope, $global) {
             $('#modal-board').modal('hide');
         }
     }
+    
+    //a cancel button, if you didn't mean to
+    $scope.getPositionCancel = function(){
+        $scope.activeplayer = null;
+        $('#modal-board').modal('hide');
+    };
+    
 
     // load strategy for editing
     $scope.loadStrat = function (strat) {
